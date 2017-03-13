0b89b7e509b0a20bb845cb8a0ec96c2010eca150
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-03-13 09:33:49 -0400

    Make pipeline work for etl-cli

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var Promise = require('bluebird'),
-    etl = require('etl'),
-    path = require('path'),
-    crypto = require('crypto'),
-    expat = require('node-expat'),
-    Stream = require('stream'),
-    request = Promise.promisifyAll(require('request'),{multiArgs:true}),
-    cheerio = require('cheerio'),
-    globalEmitter = new (require('events'))();
+const Promise = require('bluebird');
+const etl = require('etl');
+const path = require('path');
+const xmler = require('xmler');
+const Stream = require('stream');
+const request = Promise.promisifyAll(require('request'),{multiArgs:true});
+const cheerio = require('cheerio');
+const crypto = require('crypto');
+const globalEmitter = new (require('events'))();
 
 // Get the collection of rss links;
-var links = request.getAsync('https://www.pacer.gov/psco/cgi-bin/links.pl')
+const links = request.getAsync('https://www.pacer.gov/psco/cgi-bin/links.pl')
   .spread( (res,body) => {
     return cheerio.load(body)('a')
       .map(function() { return cheerio(this).attr('href');})
@@ -17,101 +17,84 @@ var links = request.getAsync('https://www.pacer.gov/psco/cgi-bin/links.pl')
       .filter(d => d.indexOf('rss_outside') !== -1);
   });
 
-// Readable stream that spits out sequential links in a loop
-var read = Stream.Readable({
-  objectMode: true,
-  links : [],
-  read : function()  {
-    var next = (this.links || []).pop();
-    if (!next)
+
+module.exports = argv => {
+  // Keep track of the latest guid we received for each court
+  const cache = {};
+  const out = etl.map();
+  argv = argv || {};
+
+  // Readable stream that spits out sequential links in a loop
+  return Stream.Readable({
+    objectMode: true,
+    read : function()  {
       links.then(links => {
-        this.links = links.slice();
-        this._read();
+        const val = links.shift();
+        this.push(val);
+        links.push(val);
       });
-    else
-      this.push(next);
-  }
-});
+    }
+  })
+  .pipe(etl.map(argv.concurrency || 10,url => {
+    const req = request.get({
+        url:url,
+        gzip:true,
+        rejectUnauthorized : false
+      });
 
-// Keep track of the latest guid we received for each court
-var cache = {};
+    let aborted;
 
-read.pipe(etl.map(10,function(url,cb) {
-  var req = request.get({
-      url:url,
-      gzip:true,
-      rejectUnauthorized : false
-    });
-
-  var item,elem,text,aborted;
-
-  req.pipe(new expat.Parser('UTF-8'))
-    .on('startElement',d => {
-      if (d == 'item')
-        item = {};
-      elem = d; text = '';
-    })
-    .on('endElement', (d) => {
-      if (d == 'item') {
+    return new Promise( (resolve,reject) => req
+      .pipe(xmler('item',{valuesOnly:true}))
+      .pipe(etl.map(function(item) {
         item._id = crypto.createHash('md5').update(JSON.stringify(item)).digest('hex');
         if (!cache[url])
           cache[url] = item._id;
         // If we seen the hash before it is time to stop
         else if(cache[url] == item._id) {
-          process.stdout.write('|');  // notify we have seen this before
           aborted = true;
           req.abort();
         } else {
-          if (!aborted)
-            this.push(item);
-          item = undefined;
+          item.fetched = new Date();
+          item.case = item.title && item.title.split(' ')[0];
+          var m = /\[(.*?)\]/.exec(item.description || '');
+          if (m) item.action = m[1];
+          console.log(item);
+          return item;
         }
-      } else if(item) {
-        item[elem] = text;
+      }))
+      .on('error',reject)
+      .on('finish',resolve)
+      .pipe(out)
+    );
+  }))
+  .pipe(out);
+};
+
+  // Express server
+if (!module.parent) {
+  module.exports().pipe(etl.map(item => globalEmitter.emit('event',item)));
+
+  require('express')()
+    .get('/events', (req,res) => {
+      res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+      });
+     
+      function emit(d) {
+        res.write('id: '+(new Date()).valueOf()+'\n');
+        res.write('data: '+JSON.stringify(d)+'\n\n');
+        if (typeof res.flush === 'function')
+          res.flush();
       }
+
+      globalEmitter.on('event',emit);
+      req.on('close', () =>  globalEmitter.removeListener('event',emit));
     })
-    .on('text',d => text+=d)
-    .on('error',e => {console.log(e); cb();})  // just log the error, dont stop
-    .on('end',cb);
-}))
-.pipe(etl.map(d => {
-  d.fetched = new Date();
-  d.case = d.title && d.title.split(' ')[0];
-  var m = /\[(.*?)\]/.exec(d.description || '');
-  if (m) d.action = m[1];
-  globalEmitter.emit('event',d);
-  return d;
-}))
-.pipe(etl.map(d => {
-  // upload to a database
-  process.stdout.write('.');
-}));
-
-// Express server
-
-require('express')()
-  .get('/events',function(req,res) {
-    res.writeHead(200, {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
-    });
-   
-    function emit(d) {
-      res.write('id: '+(new Date()).valueOf()+'\n');
-      res.write('data: '+JSON.stringify(d)+'\n\n');
-      if (typeof res.flush === 'function')
-        res.flush();
-    }
-
-    globalEmitter.on('event',emit);
-    req.on('close',function() {
-      globalEmitter.removeListener('event',emit);
-    });
-  })
-  .use(function(req,res) {
-    res.sendFile(path.join(__dirname,'index.html'));
-  })
-  .listen(3000);
+    .use( (req,res) => res.sendFile(path.join(__dirname,'index.html')))
+    .listen(3000);
+}
 
 
